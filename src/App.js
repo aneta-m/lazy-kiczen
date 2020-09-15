759e3d9b244f65eaf57419235f15d151a27a3cf7
unknown <aneta.miatkowska@gmail.com>
2020-09-15 10:45:23 +0200

    react structure fixed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+//import routes
+
+function App() {
+    return (
+        <div className="App">
+            blablabla
+        </div>
+    );
+}
+
+export default App;
